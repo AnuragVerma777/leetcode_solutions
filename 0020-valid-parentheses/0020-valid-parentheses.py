4e162b0712ad7f4192d844673b8c1446ceea2192
AnuragVerma777 <mrj26655@gmail.com>
2026-09-09 12:33:26 +0530

    Time: 2 ms (81.52%), Space: 12.5 MB (39.81%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+
+        for char in s:
+            if char in "([{":
+                stack.append(char)
+
+            elif char in ")]}":
+                if not stack:
+                    return False
+
+                top = stack.pop()
+
+                if char == ')' and top != '(':
+                    return False
+                if char == ']' and top != '[':
+                    return False
+                if char == '}' and top != '{':
+                    return False
+
+        return len(stack) == 0
